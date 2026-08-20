9d3811c44b20df14bb9da6372eee70b0962afadc
Klecianny Melo <klecianny.melo@quorum.us>
2026-08-20 17:47:17 -0300

    :sparkles: GET /{short_hash}

--- 1-mvp/main.py
+++ 1-mvp/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 import sqlite3
 from base62 import encode
@@ -43,3 +44,33 @@ def create_short_url(request: URLRequest):
         "original_url": request.url,
         "short_url": f"http://127.0.0.1:8000/{short_hash}"
     }
+
+@app.get("/{short_hash}")
+def redirect_to_original(short_hash: str):
+    # 1. Connect to db
+    conn = sqlite3.connect("shortener.db")
+    cursor = conn.cursor()
+
+    try:
+        # 2. Search the original url of have this hash
+        cursor.execute("SELECT original_url FROM urls WHERE short_hash = ?", (short_hash))
+        result = cursor.fetchone()
+
+        # 3. If not find anything, return error 404 (not found)
+        if result is None:
+            raise HTTPException(status_code=404, detail="URL not found.")
+
+        # 4. Get the url of the result
+        original_url = result[0]
+
+        # 5. Make the redirect (status 302: found/temporary redirect)
+        return RedirectResponse(url=original_url, status_code=302)
+
+    except HTTPException:
+        # Show the error 404 to the user
+        raise
+    except Exception as e:
+        print("Database error: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error.")
+    finally:
+        conn.close()
